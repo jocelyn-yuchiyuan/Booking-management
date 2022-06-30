a17d91c641ed8872a8de38848193d2f3e03552af
jocelyn-yuchiyuan <jocelyn19950627@yahoo.com.tw>
2022-07-01 00:03:16 +0100

    getbyid-controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -14,8 +14,22 @@ import(
 var NewBook models.Book
 func GetBook(w http.Responsewriter, r *http.Request){
 	newbooks := models.GetAllBooks()
-	res,   :=json.Marshall(newBooks)
+	res, _:=json.Marshall(newBooks)
 	w.Header().Set("Content-Type","pkglication/json")
 	w.WriteHeader(http.statusOK)
 	w.Write(res)
+}
+func GetBookById(w http.Responsewriter, r *http.Request){
+	vars :=mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err:=strconv.ParsInt(bookId,0,0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	bookDetails, _:= models.GetBookById(ID)
+	res, _ :=json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.Writeheader(http.StatusOK)
+	w.Write(res)
+
 }
